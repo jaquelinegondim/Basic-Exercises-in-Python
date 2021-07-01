fdf5458d4c12e0ac058bdb63e640c1fedbec5cba
Jaqueline Gondim <62260213+jaquelinegondim@users.noreply.github.com>
2021-07-01 00:20:06 -0300

    Add new exercises
    Add Ex80

--- 100-Exercises-Guanabara/Ex76-80.py
+++ 100-Exercises-Guanabara/Ex76-80.py
@@ -61,3 +61,43 @@ else:
     print("Posição dos múltiplos de 2:", end=" ")
     for i in position:
         print(i + 1, end=" ")
+
+''' 80) Faça um algoritmo que preencha um vetor de 30 posições com números entre 1 e 15 sorteados pelo computador. Depois disso, peça para o usuário digitar um número (chave) e seu programa deve mostrar em que posições essa chave foi encontrada. Mostre também quantas vezes a chave foi sorteada. '''
+
+print()
+print("\nQuestão 80\n")
+x = []
+soma = 0
+position = []
+# Sorteando os números
+for i in range(30):
+    x.append(random.randint(1, 15))
+chave = int(input("Digite um número entre 1 e 15: "))
+
+# Testando resposta do usuário
+def test(choice):
+    while True:
+        if choice >= 1 and choice <= 15:
+            break
+        else:
+            print("Você precisa digitar um número entre 1 e 15. Tente novamente.")
+            choice = int(input("Digite um número entre 1 e 15: "))
+    return choice
+
+chave = test(chave)
+print("Números sorteados:", x)
+# Obtendo a posição da chave e quantas vezes ela foi sorteada
+j = 0
+for i in x:
+    if i == chave:
+        value = x.index(i, j) + 1 # posição da chave
+        position.append(value) # add posição na lista
+        j = value # alterando valor de j para que a função index conte a partir da posição seguinte
+        soma += 1
+
+if chave not in x:
+    print("A chave não foi sorteada.")
+else:
+    print("A chave foi sorteada", soma, "vezes.")
+    print("Posição da(s) chave(s):", end=" ")
+    print(position)
