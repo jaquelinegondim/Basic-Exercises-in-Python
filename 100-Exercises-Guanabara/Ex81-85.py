dc13c99b4f1b6830b73c2be4d4dbbb1a3700ca40
Jaqueline Gondim <62260213+jaquelinegondim@users.noreply.github.com>
2021-07-03 22:47:03 -0300

    Add new exercises
    Ex82-85

--- 100-Exercises-Guanabara/Ex81-85.py
+++ 100-Exercises-Guanabara/Ex81-85.py
@@ -39,3 +39,84 @@ print("Posições com a maior idade:", position)
         b) Quantos alunos estão acima da média da turma
         c) Qual foi a maior nota digitada
         d) Em que posições a maior nota aparece '''
+
+print("\nQuestão 82\n")
+x = []
+media = 0
+for i in range(10):
+    nota = float(input("Qual a nota do aluno? "))
+    x.append(nota)
+    media += nota
+media = media / 10
+maiorNota = max(x)
+excel = 0
+k = 0
+position = []
+for i in x:
+    if i > media:
+        excel += 1
+    if i == maiorNota:
+        place = x.index(i, k) + 1
+        position.append(place)
+        k = place
+print("\nTodas as notas:", x)
+print("Média da turma:", round(media, 2))
+print("Qtd de alunos acima da média:", excel)
+print("Maior nota:", maiorNota) 
+print("Posições em que a maior nota aparece:", position)
+
+''' 83) [DESAFIO] Crie uma lógica que preencha um vetor de 20 posições com números aleatórios (entre 0 e 99) gerados pelo computador. Logo em seguida, mostre os números gerados e depois coloque o vetor em ordem crescente, mostrando no final os valores ordenados. '''
+
+print("\nQuestão 83\n")
+import random
+vetor = []
+for i in range(20):
+    vetor.append(random.randint(0, 99))
+print("Números gerados:", vetor)
+print("Números ordenados:", sorted(vetor))
+
+''' 84) Crie um programa que leia o nome e a idade de 9 pessoas e guarde esses valores em dois vetores, em posições relacionadas. No final, mostre uma listagem contendo apenas os dados das pessoas menores de idade. '''
+
+print("\nQuestão 84\n")
+# https://stackoverflow.com/questions/8356501/python-format-tabular-output
+from tabulate import tabulate
+nomes = []
+idades = []
+table = []
+for i in range(9):
+    nomes.append(input("Digite o seu nome: "))
+    idades.append(int(input("Digite a sua idade: ")))
+    if idades[i] < 18:
+        table.append([nomes[i], idades[i]])
+if table != []:
+    print("\nPessoas menores de idade:")
+    print(tabulate(table))
+
+''' 85) Faça um algoritmo que leia o nome, o sexo e o salário de 5 funcionários e guarde esses dados em três vetores. No final, mostre uma listagem contendo apenas os dados das funcionárias mulheres que ganham mais de R$5 mil. '''
+
+# Testando se o usuário digitou a letra correta 
+def test(choice):
+    while True:
+        if choice == "F" or choice == "M":
+            break
+        else:
+            print("Você precisa escolher F para Feminino ou M para Masculino. Tente de novo!")
+            choice = input("Qual o seu gênero? [F/M] ")
+    return choice
+
+print("\nQuestão 85\n")
+nome = []
+genero = []
+salario = []
+table = []
+for i in range(5):
+    nome.append(input("Digite o seu nome: "))
+    resposta = input("Qual o seu gênero? [F/M] ")
+    resposta = test(resposta)
+    genero.append(resposta)
+    salario.append(float(input("Qual o seu salário? R$")))
+    if genero[i] == "F" and salario[i] > 5000:
+        table.append([nome[i], genero[i], "R$" + str(round(salario[i], 2))])
+if table != []:
+    print("\nNome | Gênero | Salário")
+    print(tabulate(table))
