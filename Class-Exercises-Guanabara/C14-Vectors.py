a7a185b0848e9fcbba90d45bdf5a291112817328
Jaqueline Gondim <62260213+jaquelinegondim@users.noreply.github.com>
2021-07-22 23:30:02 -0300

    Add new exercise

--- Class-Exercises-Guanabara/C14-Vectors.py
+++ Class-Exercises-Guanabara/C14-Vectors.py
@@ -64,3 +64,59 @@ for j in range(3):
 
 print("--------------------")
 print("Média da turma: " + str(round(media, 2)))
+
+''' Exercício 03: Reservar espaço no cinema '''
+
+# Funções
+
+# Checar se o número da reserva está entre 1 e 10
+def Check(reserva):
+    while True:
+        if reserva < 1 or reserva > 10:
+            print("Assento não existe. Escolha um número entre 1 e 10.")
+            reserva = int(input("Reservar a cadeira: B"))
+        else:
+            break
+    return reserva
+
+# Checar se o assento está ocupado
+def Teste(reserva):
+    while True:
+        if reservas[reserva - 1] == "---":
+            print("Assento já está ocupado. Escolha outro.")
+            reserva = int(input("Reservar a cadeira: B"))
+            reserva = Check(reserva)
+        else:
+            reservas[reserva - 1] = "---"
+            break
+
+# Checar se quer reservar outro assento
+def Continuar(escolha):
+    while True:
+        if escolha == "S" or escolha == "N":
+            break
+        else:
+            print("Você precisa digitar S para Sim ou N para Não. Tente novamente.")
+            escolha = input("Deseja continuar? [S/N] ")
+    return escolha
+
+reservas = ["B 1", "B 2", "B 3", "B 4", "B 5", "B 6", "B 7", "B 8", "B 9", "B10"]
+
+print("\nRESERVAS NO CINEMA\n")
+
+for i in range(10):
+    print("[" + reservas[i] + "]", end=" ")
+print("\n------------------------------------------------------------")
+
+while True:
+    cadeira = int(input("Reservar a cadeira: B"))
+    cadeira = Check(cadeira)
+    cadeira = Teste(cadeira)
+    print()
+    for i in range(10):
+        print("[" + reservas[i] + "]", end=" ")
+    print("\n------------------------------------------------------------")
+    exit = input("Deseja continuar? [S/N] ")
+    exit = Continuar(exit)
+    if exit == "N":
+        break
